e3310c6e0566a89fa93ca9b29bcb39c475ddedbe
XifeiNi <cecilia990@outlook.com>
2019-06-18 00:53:06 +1000

    a hard question

---
+++ 86_binary_search_tree_iterator.py
@@ -0,0 +1,50 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+Example of iterate a tree:
+iterator = BSTIterator(root)
+while iterator.hasNext():
+    node = iterator.next()
+    do something for node 
+"""
+
+
+class BSTIterator:
+    """
+    @param: root: The root of binary tree.
+    """
+    def __init__(self, root):
+        # do intialization if necessary
+        self.stack = []
+        while root != None:
+            self.stack.append(root)
+            root = root.left
+
+    """
+    @return: True if there has next node, or false
+    """
+    def hasNext(self):
+        return len(self.stack) > 0
+        # write your code here
+
+    """
+    @return: return next node
+    """
+    def next(self):
+        node = self.stack[-1]
+        if node.right is not None:
+            n = node.right
+            while n != None:
+                self.stack.append(n)
+                n = n.left
+        else:
+            n = self.stack.pop()
+            while self.stack and n == self.stack[-1].right:
+                n = self.stack.pop()
+        return node
+        # write your code here
+
