e2cd9c82c788a6a1f3caff4edc037dbf9edf3cc3
XifeiNi <cecilia990@outlook.com>
2019-08-07 15:30:43 +1000

    由于是排序数组，两次二分查找，一个是第一次比target-1大的位置，一个是第一次比他target大的位置。返回即可 be mindful that should return array [end] at the second binary search

---
+++ 61_search_for_a_range.py
@@ -0,0 +1,38 @@
+class Solution:
+    """
+    @param A: an integer sorted array
+    @param target: an integer to be inserted
+    @return: a list of length 2, [index1, index2]
+    """
+    def searchRange(self, A, target):
+        # write your code here
+        if len(A) == 0:
+            return [-1, -1]
+        
+        start, end = 0, len(A) - 1
+        while start + 1 < end:
+            mid = (start + end)//2
+            if A[mid] < target:
+                start = mid
+            else:
+                end = mid
+        if A[start] == target:
+            leftBound = start
+        elif A[end] == target:
+            leftBound = end
+        else:
+            return [-1, -1]
+            
+        start, end = 0, len(A) -1
+        while start + 1 < end:
+            mid = (start + end)//2
+            if A[mid] <= target:
+                start = mid
+            else:
+                end = mid
+        if A[end] == target:
+            rightBound = end
+        else:
+            rightBound = start
+        return [leftBound, rightBound]
+
