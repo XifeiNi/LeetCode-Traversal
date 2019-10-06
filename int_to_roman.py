e12b61b1786179bda2261b83031609942c42bd88
XifeiNi <cecilia990@outlook.com>
2019-10-06 23:05:44 +1100

    Brute force but worked

---
+++ int_to_roman.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return M[num//1000] + C[(num//100)%10] + X[(num//10)%10] + I[num%10]
+    
+        
+        
