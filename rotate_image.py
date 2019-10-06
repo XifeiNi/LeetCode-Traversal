eb04a9c24caa71113357703697180500f200e6a7
XifeiNi <cecilia990@outlook.com>
2019-10-06 22:53:49 +1100

    In place rotate image

---
+++ rotate_image.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(len(matrix)):
+            matrix[i].reverse()
