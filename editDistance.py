04529d28851ac5d50e4db89852149166006ba232
XifeiNi <cecilia990@outlook.com>
2019-11-04 22:42:38 +1100

    matching type DP

---
+++ editDistance.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n, m = len(word1), len(word2)
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        
+        for i in range(n+1):
+            dp[i][0] = i
+        for j in range(m+1):
+            dp[0][j] = j
+        
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+                else:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+        return dp[n][m]
