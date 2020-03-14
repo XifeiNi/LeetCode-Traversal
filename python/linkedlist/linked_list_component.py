fa8369cad4e144723aa2fe5afcdabfbef240a87d
XifeiNi <cecilia990@outlook.com>
2020-03-14 22:53:21 +1100

    LeetCode 817 Linked List Components 判断链表里面有几个节点是“connected components”，单个的节点在数组中出现算一个“connected components”，相连的几个节点在数组中出现，只算一个“connected components”。 我们可以用二分法查找当前节点是否在数组中出现，如果当前节点的后继节点同样存在，则将指针后移，如果不存在则res++。这里要加一个flag，判断之前的节点是否存在数组中。

---
+++ python/linkedlist/linked_list_component.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def numComponents(self, head: ListNode, G: List[int]) -> int:
+        setG = set(G)
+        res = 0
+        while head:
+            if head.val in setG and (head.next == None or head.next.val not in setG):
+                res += 1
+            head = head.next
+        return res
