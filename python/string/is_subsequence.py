4661e2ac7b136e5d3aa30806c8bcac12c4562907
XifeiNi <cecilia990@outlook.com>
2020-03-14 22:45:28 +1100

    LeetCode 392: Is Sebsequence 直接按s串的顺序去遍历t串即可。

---
+++ python/string/is_subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_index = 0
+        t_index = 0
+        while s_index < len(s) and t_index < len(t):
+            if s[s_index] == t[t_index]:
+                s_index += 1
+            t_index += 1
+        if s_index == len(s):
+            return True
+        else:
+            return False
