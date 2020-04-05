21268cd5c28ef65877d09e7547adf2a0d0c830e4
XifeiNi <cecilia990@outlook.com>
2020-04-06 01:54:11 +1000

    LeetCode 1004. Max Consecutive Ones III

---
+++ python/two_pointers/maxConsecutiveOnes.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestOnes(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        i = 0
+        for j in xrange(len(A)):
+            K -= 1 - A[j]
+            if K < 0:
+                K += 1 - A[i]
+                i += 1
+        return j - i + 1
+        
